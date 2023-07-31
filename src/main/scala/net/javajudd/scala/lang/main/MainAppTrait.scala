83daa0bef69a27a7931d112d78c014f3532d903b
Christopher M. Judd <javajudd@gmail.com>
2023-07-31 09:35:51 -0400

    Main Method Alternative Examples.

---
+++ src/main/scala/net/javajudd/scala/lang/main/MainAppTrait.scala
@@ -0,0 +1,5 @@
+package net.javajudd.scala.lang.main
+
+object MainAppTrait extends App {
+  println("Scala 2 App trait main: " + args.mkString(","))
+}
---
+++ src/main/scala/net/javajudd/scala/lang/main/MainStandard.scala
@@ -0,0 +1,7 @@
+package net.javajudd.scala.lang.main
+
+object MainStandard {
+  def main(args: Array[String]): Unit = {
+    println("Scala 2 Standard main: " + args.mkString(","))
+  }
+}
