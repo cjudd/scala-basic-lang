9923308425da8df581939f5008a49e0e2c649030
Christopher M. Judd <javajudd@gmail.com>
2023-08-03 16:31:44 -0400

    Added println examples.

---
+++ src/main/scala/net/javajudd/scala/lang/print/Print.scala
@@ -0,0 +1,10 @@
+package net.javajudd.scala.lang.print
+
+object Print extends App {
+
+  val pi: Double = 3.14159265359
+
+  println("Pi equals " + pi)
+
+  println(f"Pi equals $pi%.3f") // System.out.format()
+}
