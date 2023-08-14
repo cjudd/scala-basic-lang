5260e80713abdc6600f38a8a90c3c572a381278e
Christopher M. Judd <javajudd@gmail.com>
2023-08-14 16:21:39 -0400

    Example of common types and metadata about them.

---
+++ src/main/scala/net/javajudd/scala/lang/types/Types.scala
@@ -0,0 +1,20 @@
+package net.javajudd.scala.lang.types
+
+object Types extends App {
+
+  val list: List[Any] = List(
+    "a string",
+    732,
+    12.34,
+    12.34f,
+    'c',
+    '☺', //\u263a
+    true,
+    this,
+    Unit,
+    (),
+    () => "an anonymous function returning a string"
+  )
+
+  list.foreach(e => println(s"$e - ${e.getClass} - ${e.getClass.getSuperclass}"))
+}
