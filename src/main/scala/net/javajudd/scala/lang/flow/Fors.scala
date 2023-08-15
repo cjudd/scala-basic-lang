436a5a9085f480beab95b91d3db2bbc09134972a
Christopher M. Judd <javajudd@gmail.com>
2023-08-15 10:34:52 -0400

    Added For loop examples.

---
+++ src/main/scala/net/javajudd/scala/lang/flow/Fors.scala
@@ -0,0 +1,23 @@
+package net.javajudd.scala.lang.flow
+
+object Fors extends App {
+
+  for (i <- List(1, 2, 3)) println(i)
+
+  val ints = List(1, 2, 3, 4, 5)
+  for (i <- ints) {
+    println(i)
+  }
+
+  for(i <- ints if i > 2) { // guards
+    println(i)
+  }
+
+  for {
+    i <- 1 to 3
+    j <- 'a' to 'f'
+  } {
+    println(s"${i}, ${j}")
+  }
+
+}
