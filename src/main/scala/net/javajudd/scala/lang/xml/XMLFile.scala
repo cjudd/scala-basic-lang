77f19b40194ff603908226df30867eaf7297a2da
Christopher M. Judd <javajudd@gmail.com>
2023-08-16 16:13:28 -0400

    Cleaned up XML example.

--- src/main/scala/net/javajudd/scala/lang/xml/XMLFile.scala
+++ src/main/scala/net/javajudd/scala/lang/xml/XMLFile.scala
@@ -1,15 +1,15 @@
 package net.javajudd.scala.lang.xml
 
-import scala.xml.XML
-import java.io.InputStream
+import scala.xml.{Elem, Node, XML}
+import scala.xml.transform.{RewriteRule, RuleTransformer}
 
 object XMLFile extends App {
 
-  val inputStream : InputStream = this.getClass.getResourceAsStream("/50782.xml")
+  val inputStream = this.getClass.getResourceAsStream("/50782.xml")
   val xml = XML.load(inputStream)
 
   val uri = (xml \ "substance-uri").text
-  val rn = (xml \ "substance-uri" \ "@registry-number").text
+  val rn = (xml \ "substance-uri" \ "@registry-number")
   val diplayName = (xml \ "display-name").text
   val formula = (xml \ "molecular-formula" \ "formula").text
   val synonyms = (xml \ "substance-name" \ "name").map(x => x.text).toSet.mkString("; ")
@@ -31,4 +31,21 @@ object XMLFile extends App {
     println(s"\t${name.text}")
   }
 
+  val newName = <name>Judd</name>
+  val newXml = new RuleTransformer(new AddChildrenTo("substance-name", newName)).transform(xml).head
+
+  XML.save("/tmp/chem.xml", newXml)
+}
+
+class AddChildrenTo(label: String, newChild: Node) extends RewriteRule {
+  override def transform(n: Node) = n match {
+    case n @ Elem(_, `label`, _, _, _*) => addChild(n, newChild)
+    case other => other
+  }
+
+  def addChild(n: Node, newChild: Node) = n match {
+    case Elem(prefix, label, attribs, scope, child@_*) =>
+      Elem(prefix, label, attribs, scope, false, child ++ newChild: _*)
+    case _ => sys.error("Can only add children to elements!")
+  }
 }
