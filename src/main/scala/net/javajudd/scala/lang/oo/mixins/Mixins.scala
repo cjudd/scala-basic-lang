ec73b530e6427958c33a42e62abd205b35b3dbff
Christopher M. Judd <javajudd@gmail.com>
2023-08-22 14:56:54 -0400

    Added instance mixin example(s).

--- src/main/scala/net/javajudd/scala/lang/oo/mixins/Mixins.scala
+++ src/main/scala/net/javajudd/scala/lang/oo/mixins/Mixins.scala
@@ -13,8 +13,13 @@ class Person extends Employee with Musician {
   override def work(): Unit = println("Working...")
 }
 
+class Dog
+
 object Mixins extends App {
   val person = new Person
   person.work()
   person.play()
+
+  val spot = new Dog with Musician
+  spot.play()
 }
