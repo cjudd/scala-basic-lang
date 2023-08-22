f656b6936f2c233aa94b69af737f253d92fe06f9
Christopher M. Judd <javajudd@gmail.com>
2023-08-22 15:00:27 -0400

    Added exclusive range example(s).

--- src/main/scala/net/javajudd/scala/lang/types/Ranges.scala
+++ src/main/scala/net/javajudd/scala/lang/types/Ranges.scala
@@ -2,9 +2,16 @@ package net.javajudd.scala.lang.types
 
 object Ranges extends App {
 
-  val range = 1 to 10
+  val inclusiveRange = 1 to 10
+  val exclusiveRange = 1 until 10
 
+  println("Inclusive:")
   for (i <- 1 to 10) {
+    println(i)
+  }
+
+  println("Exclusive:")
+  for (i <- 1 until 10) {
     println(i)
   }
 
