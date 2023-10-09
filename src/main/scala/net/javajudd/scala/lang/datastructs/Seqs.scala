2dc6665c6456d96269c1480d467591055463aad7
Christopher M. Judd <javajudd@gmail.com>
2023-10-09 11:12:17 -0400

    Removed redundant example of seq.

--- src/main/scala/net/javajudd/scala/lang/datastructs/Seqs.scala
+++ src/main/scala/net/javajudd/scala/lang/datastructs/Seqs.scala
@@ -3,8 +3,6 @@ package net.javajudd.scala.lang.datastructs
 object Seqs extends App {
 
   val seq = Seq(1, 2, 3)
-  println(s"Head: ${seq.head}")
-  println(s" ${seq.head}")
 
   println(s"first element: ${seq(1)}")
   println(s"head: ${seq.head}")
